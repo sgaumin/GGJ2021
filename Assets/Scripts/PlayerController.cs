c4f25504dce4aabfc3958f87f2d6c0dc77dd1325
Sebastien Gm <sgaumin@gmail.com>
2021-01-31 08:57:54 +0900

    [Feature] Player Movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,91 +2,96 @@
 
 public class PlayerController : MonoBehaviour
 {
-    // https://www.immersivelimit.com/tutorials/simple-character-controller-for-unity
-    Rigidbody rb;
-    public HidingPlace hidingPlace;
-    Animator animator;
-    bool isCrouching;
+	// https://www.immersivelimit.com/tutorials/simple-character-controller-for-unity
+	Rigidbody rb;
+	public HidingPlace hidingPlace;
+	Animator animator;
+	bool isCrouching;
 
-    public float turnSpeed = 20f;
-    Quaternion rotation = Quaternion.identity;
+	public float turnSpeed = 20f;
+	Quaternion rotation = Quaternion.identity;
 
-    public float moveSpeed;
-    public float horizontal;
-    public float vertical;
+	public float moveSpeed;
+	public float horizontal;
+	public float vertical;
 
-    public bool CanHide;
-    public bool IsHidden;
+	public bool CanHide;
+	public bool IsHidden;
 
-    public GameObject model;
+	public GameObject model;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-        animator = GetComponentInChildren<Animator>();
-        
-        hidingPlace = null;
+	// Start is called before the first frame update
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+		animator = GetComponentInChildren<Animator>();
 
-    }
+		hidingPlace = null;
 
-    // Update is called once per frame
-    void FixedUpdate()
-    {
+	}
 
-        ProcessActions();
+	// Update is called once per frame
+	void FixedUpdate()
+	{
 
-        horizontal = Input.GetAxis("Horizontal");
-        vertical = Input.GetAxis("Vertical");
+		ProcessActions();
 
-        if (Input.GetKey(KeyCode.Space))
-        {
-            if (CanHide)
-            {
-                isCrouching = true;
-                //UIManager.Instance.SetText("Player is Hiding");
-                IsHidden = true;
-                //model.SetActive(false);
-                hidingPlace.launchTimer = true;
-            }
-            else
-            {
-                model.SetActive(true);
-                isCrouching = false;
-                IsHidden = false;
-            }
-        }
-        else
-        {
-            model.SetActive(true);
-            isCrouching = false;
-            IsHidden = false;
-        }
+		horizontal = Input.GetAxis("Horizontal");
+		vertical = Input.GetAxis("Vertical");
 
-        
+		if (Input.GetKey(KeyCode.Space))
+		{
+			if (CanHide)
+			{
+				isCrouching = true;
+				//UIManager.Instance.SetText("Player is Hiding");
+				IsHidden = true;
+				//model.SetActive(false);
+				hidingPlace.launchTimer = true;
+			}
+			else
+			{
+				model.SetActive(true);
+				isCrouching = false;
+				IsHidden = false;
+			}
+		}
+		else
+		{
+			model.SetActive(true);
+			isCrouching = false;
+			IsHidden = false;
+		}
 
-        void ProcessActions()
-        {
-            Vector3 move = new Vector3(horizontal, 0f, vertical);
-            Vector3 desiredForward = Vector3.RotateTowards(transform.forward, move, turnSpeed * Time.deltaTime, 0f);
-            rotation = Quaternion.LookRotation(desiredForward);
-            rb.AddForce(move.normalized * moveSpeed);
-            rb.MoveRotation(rotation);
-            model.transform.Rotate(rotation.eulerAngles);
-        }
 
-        //Animation
-        //isMoving
-        bool hasHorizontalInput = !Mathf.Approximately(horizontal, 0f);
-        bool hasVerticalInput = !Mathf.Approximately(vertical, 0f);
-        bool isMoving = hasHorizontalInput || hasVerticalInput;
-        animator.SetBool("isMoving", isMoving);
 
-        //isHidden
-        animator.SetBool("isHidden", IsHidden);
+		void ProcessActions()
+		{
+			// Movements
+			Vector3 move = new Vector3(horizontal, 0f, vertical);
+			rb.AddForce(move.normalized * moveSpeed);
 
-        //isCrouching
-        animator.SetBool("isCrouching", isCrouching);
-    }
+			// Rotation
+			if (move != Vector3.zero)
+			{
+				Vector3 desiredForward = move.normalized;
+				rotation = Quaternion.LookRotation(desiredForward);
+				model.transform.rotation = Quaternion.Lerp(model.transform.rotation, rotation, 0.1f);
+			}
+		}
+
+		//Animation
+		//isMoving
+		bool hasHorizontalInput = !Mathf.Approximately(horizontal, 0f);
+		bool hasVerticalInput = !Mathf.Approximately(vertical, 0f);
+		bool isMoving = hasHorizontalInput || hasVerticalInput;
+		animator.SetBool("isMoving", isMoving);
+
+		//isHidden
+		animator.SetBool("isHidden", IsHidden);
+
+		//isCrouching
+		animator.SetBool("isCrouching", isCrouching);
+	}
 }
 
