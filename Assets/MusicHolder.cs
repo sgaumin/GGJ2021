34494a4a865c3c11560aded3f29b8cf38c8bb505
Sebastien Gm <sgaumin@gmail.com>
2021-02-01 00:26:41 +0900

    [Fix] Audio & Carpets

--- Assets/MusicHolder.cs
+++ Assets/MusicHolder.cs
@@ -7,41 +7,47 @@ public class MusicHolder : MonoBehaviour
 	[FMODUnity.EventRef, SerializeField] private string musicSound;
 
 	private static FMOD.Studio.EventInstance musicSoundInstance;
+	private bool hasBeenInialized;
 
 	protected void Awake() => Instance = this;
 
+	private void Init()
+	{
+		if (!hasBeenInialized)
+		{
+			hasBeenInialized = true;
+			musicSoundInstance = FMODUnity.RuntimeManager.CreateInstance(musicSound);
+			musicSoundInstance.start();
+		}
+	}
+
 	public void SetFirstMusic()
 	{
-		musicSoundInstance = FMODUnity.RuntimeManager.CreateInstance(musicSound);
-		musicSoundInstance.start();
+		Init();
 		musicSoundInstance.setParameterByName("Music", 0.5f);
 	}
 
 	public void SetHideMusic()
 	{
-		musicSoundInstance = FMODUnity.RuntimeManager.CreateInstance(musicSound);
-		musicSoundInstance.start();
+		Init();
 		musicSoundInstance.setParameterByName("Music", 1.5f);
 	}
 
 	public void SetChaseMusic()
 	{
-		musicSoundInstance = FMODUnity.RuntimeManager.CreateInstance(musicSound);
-		musicSoundInstance.start();
+		Init();
 		musicSoundInstance.setParameterByName("Music", 2.5f);
 	}
 
 	public void SetVictoryMusic()
 	{
-		musicSoundInstance = FMODUnity.RuntimeManager.CreateInstance(musicSound);
-		musicSoundInstance.start();
+		Init();
 		musicSoundInstance.setParameterByName("Music", 3.5f);
 	}
 
 	public void SetLooseMusic()
 	{
-		musicSoundInstance = FMODUnity.RuntimeManager.CreateInstance(musicSound);
-		musicSoundInstance.start();
+		Init();
 		musicSoundInstance.setParameterByName("Music", 4.5f);
 	}
 
