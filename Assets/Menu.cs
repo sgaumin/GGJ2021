7ec81d04f0e6f76927f8d7ea5550c1bfc30882b3
Elon <elon-elbeze@live.fr>
2021-01-31 14:46:56 +0100

    [Polish] More Game Menu

---
+++ Assets/Menu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Tools;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    public Game game;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            game.LoadNextLevel();
+        }
+    }
+}
