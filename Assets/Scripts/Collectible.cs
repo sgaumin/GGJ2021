05e554c8ff8b78713449db0b45fe87cbe5701f2d
Sebastien Gm <sgaumin@gmail.com>
2021-01-31 18:33:30 +0900

    [Feature] Last update for build 2

--- Assets/Scripts/Collectible.cs
+++ Assets/Scripts/Collectible.cs
@@ -1,43 +1,42 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Collectible : MonoBehaviour
 {
-    public GameObject enemyPrefab;
-    public float timeBeforeTransformation; //after that time, the collectible turns into an enemy
-    public float value; //value of the object for scoring
+	public GameObject enemyPrefab;
+	public float timeBeforeTransformation; //after that time, the collectible turns into an enemy
+	public float value; //value of the object for scoring
 
-    
-   
+	[Header("Debug")]
+	[SerializeField] private bool spawnEnemy;
 
-    // Update is called once per frame
-    void Update()
-    {
-        timeBeforeTransformation -= Time.deltaTime;
+	// Update is called once per frame
+	void Update()
+	{
+		if (spawnEnemy)
+		{
+			timeBeforeTransformation -= Time.deltaTime;
 
-        if (timeBeforeTransformation<=0.0f)
-        {
-            TransformIntoEnemy();//instantiate Enemy
-        }
+			if (timeBeforeTransformation <= 0.0f)
+			{
+				TransformIntoEnemy();//instantiate Enemy
+			}
+		}
+	}
 
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag("Player"))
+		{
+			var playerScore = other.GetComponent<PlayerScore>();//Increase score of value
+			playerScore.IncreaseScore(value);
 
-    }
+			Destroy(gameObject);//destroy gameobject
+		}
+	}
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if(other.gameObject.CompareTag("Player"))
-        {
-            var playerScore = other.GetComponent < PlayerScore>();//Increase score of value
-            playerScore.IncreaseScore(value);
-
-            Destroy(gameObject);//destroy gameobject
-        }
-    }
-
-    void TransformIntoEnemy()
-    {
-        Instantiate(enemyPrefab,transform.position, Quaternion.identity);
-        Destroy(gameObject);
-    }
+	void TransformIntoEnemy()
+	{
+		Instantiate(enemyPrefab, transform.position, Quaternion.identity);
+		Destroy(gameObject);
+	}
 }
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,32 +1,31 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    public GameObject objectToSpawn;
-    private float time;
-    public float spawnRate;
-    public Transform spawner;
+	public GameObject objectToSpawn;
+	private float time;
+	public float spawnRate;
+
+	private Transform spawner;
 
 
-    private void Start()
-    {
-        spawner = GetComponent<Transform>();
-        
-    }
+	private void Start()
+	{
+		spawner = GetComponent<Transform>();
+
+	}
 
 
-    private void Update()
-    {
-        time += Time.deltaTime;
+	private void Update()
+	{
+		time += Time.deltaTime;
 
-        if(time> spawnRate)
-        {
-           Instantiate(objectToSpawn, spawner.position, spawner.rotation);//Instantiate
-            time = 0f;//Reset Time
-        }
+		if (time > spawnRate)
+		{
+			Instantiate(objectToSpawn, spawner.position, spawner.rotation);//Instantiate
+			time = 0f;//Reset Time
+		}
 
-    }
+	}
 
 }
