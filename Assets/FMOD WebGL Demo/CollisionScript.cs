918e2dd56212e8ec82a207c08360ea273c83d9d7
Sebastien Gm <sgaumin@gmail.com>
2021-02-07 21:28:30 +0900

    [Feature] Loading scene for Audio creation

---
+++ Assets/FMOD WebGL Demo/CollisionScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CollisionScript : MonoBehaviour
+{
+    [FMODUnity.EventRef]
+    public string eventRef;
+
+#if UNITY_EDITOR // Used for testing in Editor since banks are loaded in a different scene.
+    private void Awake()
+    {
+        FMODUnity.RuntimeManager.LoadBank("Master Bank");
+    }
+#endif
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        FMODUnity.RuntimeManager.PlayOneShot(eventRef, collision.transform.position);
+    }
+}
---
+++ Assets/FMOD WebGL Demo/LoadBankAndScene.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadBankAndScene : MonoBehaviour
+{
+	[SerializeField]
+	public string sceneName;
+
+	[FMODUnity.BankRef]
+	public List<string> banks;
+
+	private void Awake()
+	{
+		foreach (string b in banks)
+		{
+			FMODUnity.RuntimeManager.LoadBank(b, true);
+			Debug.Log("Loaded bank " + b);
+		}
+	}
+
+	void Update()
+	{
+		if (FMODUnity.RuntimeManager.HasBankLoaded("Master"))
+		{
+			Debug.Log("Master Bank Loaded");
+			SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+		}
+	}
+
+}
